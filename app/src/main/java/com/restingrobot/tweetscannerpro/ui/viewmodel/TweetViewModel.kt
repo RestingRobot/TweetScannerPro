f40d67b56fd37a7b026b7407174b547a33c46f18
GentlemanBastard <jlange@themedicalmemory.com>
2021-05-11 14:23:53 -0700

    Add Hilt and Application class

--- app/src/main/java/com/restingrobot/tweetscannerpro/ui/viewmodel/TweetViewModel.kt
+++ app/src/main/java/com/restingrobot/tweetscannerpro/ui/viewmodel/TweetViewModel.kt
@@ -9,7 +9,6 @@ import androidx.lifecycle.AndroidViewModel
  * Common view model for all features using tweets
  *
  * Created by Jon Lange, 5/11/21
- * Copyright (C) 2021 Medical Memory LLC
  */
 class TweetViewModel constructor(application: Application) : AndroidViewModel(application) {
 }
